e33e53004abdb05ebac0bce460827c8abf0cd9e5
shrinivas333 <21shrin@gmail.com>
2021-02-01 16:28:31 +0530

    state

--- Estimation/views.py
+++ Estimation/views.py
@@ -5,6 +5,7 @@ from .models import User,AddOn,Item,Order,ItemAddon
 from rest_framework import viewsets, mixins,serializers
 from django.http import HttpResponse
 import json
+from rest_framework.decorators import action
 # Create your views here.
 
 class UserViewsets(viewsets.ModelViewSet):
@@ -22,52 +23,50 @@ class AddonViewSet(viewsets.GenericViewSet,
     
     
 
-class ItemsViewSet(viewsets.GenericViewSet,mixins.ListModelMixin,
+class ItemsViewSet(viewsets.GenericViewSet,
                     mixins.CreateModelMixin,
                     mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin):
-    queryset = Item.objects.all()
+    # queryset = Item.objects.all()
+    
     serializer_class=ItemSerializer
 
-    def get_items(self,request):
-
-        items=Item.objects.all()
-        print(items)
+   
   
-
-    # def get_queryset(self):
+    @action(detail=True, methods=['Get'])
+    def get_items(self):
         
-    #     Items=Item.objects.all()
-    #     print(Items)
-    #     items_list=[]
-    #     addons_list=[]
-    #     for item in Items:
-    #         print(item.item_id)
-    #         print(item.addons.all())
-    #         intermdediate=ItemAddon.objects.filter(item=item)
-    #         if intermdediate:
-    #             print(intermdediate.item)
-    #         try :
-    #             for addon in item.addons.all():
-    #                 quantity =ItemAddon.objects.get(item=item,addon=addon)
-    #                 print(quantity)
-    #                 addondict={"addon_id":addon.addon_id,"description":addon.description,"cost":addon.cost,"gst":addon.gst,"material":addon.material,"quantity":quantity.quantity}
+        Items=Item.objects.all()
+        print(Items)
+        items_list=[]
+        addons_list=[]
+        for item in Items:
+            print(item.item_id)
+            print(item.addons.all())
+            intermdediate=ItemAddon.objects.filter(item=item)
+            if intermdediate:
+                print(intermdediate.item)
+            try :
+                for addon in item.addons.all():
+                    quantity =ItemAddon.objects.get(item=item,addon=addon)
+                    print(quantity)
+                    addondict={"addon_id":addon.addon_id,"description":addon.description,"cost":addon.cost,"gst":addon.gst,"material":addon.material,"quantity":quantity.quantity}
 
-    #                 addons_list.append(addondict)
-    #         except:
-    #             addons_list.append(None)
-    #         iteminfo={}
-    #         iteminfo['item_id']=item.item_id
-    #         iteminfo['addons']=addons_list
-    #         iteminfo['description']=item.description
-    #         iteminfo['cost']=item.cost
-    #         iteminfo['gst']=item.gst
-    #         iteminfo['material']=item.material
+                    addons_list.append(addondict)
+            except:
+                addons_list.append(None)
+            iteminfo={}
+            iteminfo['item_id']=item.item_id
+            iteminfo['addons']=addons_list
+            iteminfo['description']=item.description
+            iteminfo['cost']=item.cost
+            iteminfo['gst']=item.gst
+            iteminfo['material']=item.material
 
-    #         items_list.append(iteminfo)
+            items_list.append(iteminfo)
 
-    #     return HttpResponse(json.dumps(items_list))
+        return HttpResponse(json.dumps(items_list))
 
     def create(self,request):
         print(request.data)
